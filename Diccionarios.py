cabd76fde13808ee66a874b79c82e93188d34197
carlafaes <carlafaes18@hotmail.com>
2022-06-27 18:44:24 -0300

    diccionarios

--- Diccionarios.py
+++ Diccionarios.py
@@ -15,3 +15,7 @@ print(diccionario)
 
 vasos={"vaso1":"rojo","vaso2":"azul","vaso3":"rosado"}
 print(vasos.get("vaso1","no se encuentra en el diccionario"))#retorna el valor del elemento si esta, sino retorna el valor por defecto
+
+print(vasos.keys())#retorna las claves del diccionario
+print(vasos.values())#retorna los valores del diccionario
+print(vasos.items())#retorna los pares clave-valor del diccionario
