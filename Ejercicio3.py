e783a1f4d83c5379d20f6769223ad57c2b97912d
carlafaes <carlafaes18@hotmail.com>
2022-06-27 20:00:50 -0300

    listas

---
+++ Ejercicio3.py
@@ -0,0 +1,20 @@
+#Ejercicio3: Escribir un programa donde cree una lista cons los siguientes personajes del señor de los anillos
+#Nombre: Aragorn
+#Clase:Guerrero
+#Raza:Dúnedain
+#Nombre:Gandalf
+#Clase:Mago
+#Raza:Istar
+#Nombre:Legolas
+#Clase:Arquero
+#Raza:Elfo Sindar
+
+personajes=[]
+p={"Nombre":"Aragorn","Clase":"Guerrero","Raza":"Dúnedain"}
+personajes.append(p)
+p={"Nombre":"Gandalf","Clase":"Mago","Raza":"Istar"}
+personajes.append(p)
+p={"Nombre":"Legolas","Clase":"Arquero","Raza":"Elfo Sindar"}
+personajes.append(p)
+print(personajes)
+
